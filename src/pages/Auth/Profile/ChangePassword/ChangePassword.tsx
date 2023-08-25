f91d52baf4d16152523cbea4b47078ebbf029481
EErmilin <eliseyermiline@gmail.com>
2023-08-25 13:53:22 +0300

    Страница смены пароля

--- src/pages/Auth/Profile/ChangePassword/ChangePassword.tsx
+++ src/pages/Auth/Profile/ChangePassword/ChangePassword.tsx
@@ -2,16 +2,38 @@ import Input from "../../../../components/UI/areas/Input/Input";
 import classes from "./ChangePassword.module.scss";
 
 
-function ChangePassword ({}){
+function ChangePassword({ }) {
 
     return (
         <div className={classes.wrp}>
             <h3 className={classes.title}>Сменить пароль</h3>
-                <div className={classes.info}>
-                <Input></Input>
-                <Input></Input>
-                <Input></Input>
-                </div>
+            <div className={classes.info}>
+                <Input
+                    classNameWrp={classes.input}
+                    placeholder='Пароль'
+                    name="password"
+                    id="password"
+                    mask={''}
+                    type={'password'}></Input>
+                <Input
+                    classNameWrp={classes.input}
+                    placeholder='Новый пароль'
+                    name="password"
+                    id="password"
+                    mask={''}
+                    type={'password'}></Input>
+                <Input
+                    classNameWrp={classes.input}
+                    placeholder='Повторите пароль'
+                    name="password"
+                    id="password"
+                    mask={''}
+                    type={'password'}></Input>
+            </div>
+            <button
+                //onClick={handleSubmit}
+                className={classes.btn}
+            >Сохранить изменения</button>
         </div>
     )
 }

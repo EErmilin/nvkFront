b2bc16cd29f5613e6cf1e84d6b4d262d6275af51
EErmilin <eliseyermiline@gmail.com>
2023-09-06 14:27:42 +0300

    BCAJ-30 Вёрстка музыки и подтягивание данных с сервера

--- src/pages/Music/Music.tsx
+++ src/pages/Music/Music.tsx
@@ -2,37 +2,86 @@ import React, { useEffect, useMemo } from 'react'
 import '../../assets/css/main.css';
 import classes from "./Music.module.scss";
 import { ReactComponent as FakeCompilation } from '../../assets/img/music.svg'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { getMusics } from '../../redux/thunks/screens/musics/GetMusics';
+import { getPodcasts } from '../../redux/thunks/screens/podcasts/GetPodcasts';
+
+
+const TAKE = 8;
 
 function Music() {
+    const musicsRedux: any = useAppSelector(state => state.screens.musics);
+    const podcastsRedux: any = useAppSelector(state => state.screens.podcasts);
+    const dispatcher = useAppDispatch()
 
-    const templateСompilations = useMemo(() => {
-        return <div>
-            <FakeCompilation />
-            <div className={classes.music_field_item_title}>Мой плейлист</div>
-        </div>
-    }, [])
+    const update = React.useCallback(async () => {
+        await dispatcher(getMusics({ take: TAKE }));
+        await dispatcher(getPodcasts({ take: TAKE }));
+    }, [dispatcher]);
+
+    React.useEffect(() => {
+        update();
+    }, [update]);
+
+    console.log('!!!!!!!')
+    console.log(musicsRedux)
+
+    const templatePlaylists = useMemo(() => {
+        return musicsRedux?.playlists.map((elem: any, key: number) => {
+            return <div>
+                {!elem.cover?.url_256 ? <div className={classes.music_field_no_img} /> : <img src={elem.cover?.url_256} className={classes.music_field_img} alt="Logo" />}
+                <div className={classes.music_field_item_title}>{elem.name}</div>
+            </div>
+        })
+    }, [musicsRedux])
 
     const templatePopulars = useMemo(() => {
-        return <div>
-            <FakeCompilation />
-            <div className={classes.music_field_item_title}>Мой плейлист</div>
-        </div>
-    }, [])
+        return musicsRedux?.albums.map((elem: any, key: number) => {
+            return <div>
+                {!elem.cover?.url_256 ? <div className={classes.music_field_no_img} /> : <img className={classes.music_field_img} src={elem.cover?.url_256} alt="Logo" />}
+                <div className={classes.music_field_item_title}>{elem.name}</div>
+            </div>
+        })
+    }, [musicsRedux])
 
     const templatePodcasts = useMemo(() => {
-        return <div>
-            <FakeCompilation />
-            <div className={classes.music_field_item_title}>Мой плейлист</div>
-        </div>
-    }, [])
+        if(!podcastsRedux)return
+        return podcastsRedux.map((elem: any, key: number) => {
+            return <div>
+                {!elem.cover?.url_256 ? <div className={classes.music_field_no_img} /> : <img className={classes.music_field_img} src={elem.cover?.url_256} alt="Logo" />}
+                <div className={classes.music_field_item_title}>{elem.name}</div>
+            </div>
+        })
+    }, [podcastsRedux])
+
+    const templatePodcastsCurrent = useMemo(() => {
+        return podcastsRedux&&podcastsRedux.length&& podcastsRedux[0].podcastAlbums.map((elem: any, key: number) => {
+
+            return <div>
+                {!elem.cover?.url_256 ? <div className={classes.music_field_no_img} /> : <img className={classes.music_field_img} src={elem.cover?.url_256} alt="Logo" />}
+                <div className={classes.music_field_item_title}>{elem.name}</div>
+            </div>
+        })
+    }, [podcastsRedux])
+
+    console.log('!!!!!!')
+    console.log(podcastsRedux)
+
 
     const templateMusics = useMemo(() => {
-        return <div className={classes.music_field_music_item}>
-            <div className={classes.music_field_music_img}>
+        if(!musicsRedux)return
+        return musicsRedux.songs.map((elem: any, key: number) => {
+
+            return <div className={classes.music_field_music_item}>
+                {!elem.cover?.url_256 ? <div className={classes.music_field_music_item_no_img} /> : <img className={classes.music_field_music_item_img} src={elem.cover?.url_256} alt="Logo" />}
+                <div className={classes.music_field_music_item_wrp}>
+                    <div className={classes.music_field_music_item_title}>{elem.title} </div>
+                    <div className={classes.music_field_music_item_name}>{elem.artist.name}</div>
+                    <div className={classes.music_field_music_item_btn} />
+                </div>
             </div>
-            <div className={classes.music_field_music_title}>Мой плейлист</div>
-        </div>
-    }, [])
+        })
+    }, [musicsRedux])
 
     return (
         <div className={classes.music}>
@@ -42,7 +91,7 @@ function Music() {
                     <div className={classes.music_field_header_btn}>Все</div>
                 </div>
                 <div className={classes.music_field_wrp}>
-                    {templateСompilations}
+                    {templatePlaylists}
                 </div>
 
             </div>
@@ -61,7 +110,7 @@ function Music() {
                     <h1 className={classes.music_field_title}>Новые треки</h1>
                     <div className={classes.music_field_header_btn}>Все</div>
                 </div>
-                <div className={classes.music_field_wrp}>
+                <div className={classes.music_field_wrp_music}>
                     {templateMusics}
                 </div>
 
@@ -82,7 +131,7 @@ function Music() {
                     <div className={classes.music_field_header_btn}>Все</div>
                 </div>
                 <div className={classes.music_field_wrp}>
-                    {templatePodcasts}
+                    {templatePodcastsCurrent}
                 </div>
 
             </div>

cdd14d8a12179d80de53731071e00e0d4119ceff
Chereshka <wallhack2.0@gmail.com>
2023-09-19 00:52:18 +0300

    add covers, fix shedule and askQuestion textArea

--- src/components/UI/blocks/liveStream/LiveStreamSelector/LiveStreamSelector.tsx
+++ src/components/UI/blocks/liveStream/LiveStreamSelector/LiveStreamSelector.tsx
@@ -1,93 +1,111 @@
-import { forwardRef, useCallback, useEffect, useImperativeHandle, useMemo, useState } from 'react'
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from "react";
 
-import { ILive } from '../../../../../models/LiveStream';
-import { IRadio } from '../../../../../models/Radio';
+import { ILive } from "../../../../../models/LiveStream";
+import { IRadio } from "../../../../../models/Radio";
+import nvkLogo from "../../../../../assets/img/nvk.svg";
+import mamont from "../../../../../assets/img/mamont.svg";
+import yakutia from "../../../../../assets/img/yakutia.svg";
+import teteam from "../../../../../assets/img/teteam.svg";
 
-import { getUpdateClient } from '../../../../../requests/updateHeaders';
+import "./LiveStreamSelector.css";
 
-import { LIVESTEAMS } from '../../../../../gql/query/livestreams/LiveStreams';
-import { RADIOS } from '../../../../../gql/query/Radios';
+const TMP_IMAGE = (index: number) => {
+  switch (index) {
+    case 0:
+      return nvkLogo;
 
-import "./LiveStreamSelector.css"
+    case 1:
+      return mamont;
+
+    case 2:
+      return yakutia;
+
+    default:
+      return teteam;
+  }
+};
 
 type TLiveStreamCardProps = {
-    index: number,
-    stream: ILive | IRadio;
-    selected?: boolean;
-    onSelect: (index: number) => void;
-}
+  index: number;
+  stream: ILive | IRadio;
+  selected?: boolean;
+  onSelect: (index: number) => void;
+};
 
+const LiveStreamCard = ({
+  index,
+  stream,
+  selected,
+  onSelect: onSelectInner,
+}: TLiveStreamCardProps) => {
+  const onSelect = useCallback(() => onSelectInner(index), []);
 
-const LiveStreamCard = (
-    {
-        index,
-        stream,
-        selected,
-        onSelect: onSelectInner
-    }: TLiveStreamCardProps
-) => {
-
-    const onSelect = useCallback(() =>
-        onSelectInner(index), [])
-
-    return (
-        <div className="live-stream-card" onClick={onSelect} style={{ backgroundImage: `url(${stream.cover})` }}>
-            <div className="group">
-                {!selected &&
-                    <div className="LIVE">
-                        <div className="overlap-group">
-                            <div className="ellipse" />
-                            <div className="text-wrapper">LIVE</div>
-                        </div>
-                    </div>
-                }
+  return (
+    <div
+      className="live-stream-card"
+      onClick={onSelect}
+      style={{
+        backgroundImage: `url(${TMP_IMAGE(index)})`,
+        backgroundSize: "100% auto",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="group">
+        {!selected && (
+          <div className="LIVE">
+            <div className="overlap-group">
+              <div className="ellipse" />
+              <div className="text-wrapper">LIVE</div>
             </div>
-        </div >
-    );
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 type TLiveStreamBlockProps = {
-    onSelect: (stream: ILive | IRadio) => void;
-}
+  onSelect: (stream: ILive | IRadio) => void;
+};
 
 export type LiveStreamSelectorHandle = {
-    setStreams: React.Dispatch<React.SetStateAction<(ILive | IRadio)[]>>
+  setStreams: React.Dispatch<React.SetStateAction<(ILive | IRadio)[]>>;
+};
 
-}
-
-
-const LiveStreamSelector = forwardRef(({ onSelect }: TLiveStreamBlockProps, ref) => {
-
+const LiveStreamSelector = forwardRef(
+  ({ onSelect }: TLiveStreamBlockProps, ref) => {
     const [selectedStreamIndex, setSelectedStreamIndex] = useState(0);
     const [streams, setStreams] = useState<Array<ILive | IRadio>>([]);
 
     useImperativeHandle<unknown, LiveStreamSelectorHandle>(ref, () => ({
-        setStreams
+      setStreams,
     }));
 
     useEffect(() => {
-        if (streams[selectedStreamIndex])
-            onSelect(streams[selectedStreamIndex])
-    }, [streams, selectedStreamIndex])
+      if (streams[selectedStreamIndex]) onSelect(streams[selectedStreamIndex]);
+    }, [streams, selectedStreamIndex]);
 
     return (
+      <div className="stream-list">
+        {streams.map((element, index) => (
+          <LiveStreamCard
+            key={element.id}
+            index={index}
+            stream={element}
+            onSelect={setSelectedStreamIndex}
+            selected={index === selectedStreamIndex}
+          />
+        ))}
+      </div>
+    );
+  }
+);
 
-
-        <div className="stream-list">
-            {
-                streams.map((element, index) =>
-                    <LiveStreamCard
-                        key={element.id}
-                        index={index}
-                        stream={element}
-                        onSelect={setSelectedStreamIndex}
-                        selected={index === selectedStreamIndex}
-                    />
-                )
-            }
-        </div>
-
-    )
-});
-
-export default LiveStreamSelector
+export default LiveStreamSelector;
